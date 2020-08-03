e6217d854232189072e35a5ec076025bc119bad9
musiciancoder <andresguitarra1980@gmail.com>
2020-08-03 13:09:41 -0400

    1
    293. Creando la Edición de Productos y Listado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Productos from "./components/Productos";
+import NuevoProductos from "./components/NuevoProducto";
+import EditarProducto from "./components/EditarProducto";
 
-function App() {
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+function App() { //git add *
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <Router>
+    <Header />
+    <div className="container">
+      <Switch>
+        <Route exact path={"/"} component={Productos} />
+        <Route exact path={"/productos/nuevo"} component={NuevoProductos} />
+        <Route exact path={"/productos/editar/:id"} component={EditarProducto} />
+      </Switch>
     </div>
+
+  </Router>
   );
 }
 
---
+++ src/components/Productos.js
@@ -0,0 +1,29 @@
+import React , {Fragment} from 'react';
+
+const Productos = () => {
+
+    return(
+
+<Fragment>
+
+    <table className="table table-striped">
+        <thead className="bg-primary table-dark">
+        <tr>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+            <th scope="col">Acciones</th>
+        </tr>
+        </thead>
+        <tbody>
+
+        </tbody>
+    </table>
+
+</Fragment>
+
+    );
+
+};
+
+export default  Productos;
+
