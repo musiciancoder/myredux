c6c028acf35821b01acddfec869fe2098581588a
musiciancoder <andresguitarra1980@gmail.com>
2020-08-07 11:59:44 -0400

    318.Creando un segundo reduce

---
+++ src/reducers/alertaReducer.js
@@ -0,0 +1,20 @@
+import {MOSTRAR_ALERTA,
+OCULTAR_ALERTA} from "../types";
+
+//Cada reducer tiene su state
+
+
+
+const initialState = {
+    alerta:null
+}
+
+export default function (state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+
+
+    }
+
+}
